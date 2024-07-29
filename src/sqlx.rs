dff33582bc84e5bd15dbbdd45035695bff6f744c
Jacob Wiltse <kelwing@kelnet.org>
2024-07-29 12:15:20 -0400

    fix sqlx typeinfo implementation

--- src/sqlx.rs
+++ src/sqlx.rs
@@ -1,4 +1,4 @@
-use sqlx::{encode::IsNull, Decode, Encode, MySql, Postgres, Sqlite, Type};
+use sqlx::{encode::IsNull, Decode, Encode, MySql, Postgres, Sqlite, Type, Database};
 use crate::Snowflake;
 
 impl<'q> Encode<'q, MySql> for Snowflake {
@@ -30,38 +30,38 @@ impl <'q> Encode<'q, Sqlite> for Snowflake {
     }
 }
 
-impl<'r, 'b> Decode<'r, MySql> for Snowflake {
+impl<'r> Decode<'r, MySql> for Snowflake {
     fn decode(value: <MySql as sqlx::Database>::ValueRef<'r>) -> Result<Self, sqlx::error::BoxDynError> {
         Ok(Self(<u64 as Decode<MySql>>::decode(value)?))
     }
 }
 
-impl<'r, 'b> Decode<'r, Postgres> for Snowflake {
+impl<'r> Decode<'r, Postgres> for Snowflake {
     fn decode(value: <Postgres as sqlx::Database>::ValueRef<'r>) -> Result<Self, sqlx::error::BoxDynError> {
         Ok(Self(<i64 as Decode<Postgres>>::decode(value)? as u64))
     }
 }
 
-impl <'r, 'b> Decode<'r, Sqlite> for Snowflake {
+impl <'r> Decode<'r, Sqlite> for Snowflake {
     fn decode(value: <Sqlite as sqlx::Database>::ValueRef<'r>) -> Result<Self, sqlx::error::BoxDynError> {
         Ok(Self(<i64 as Decode<Sqlite>>::decode(value)? as u64))
     }
 }
 
 impl Type<MySql> for Snowflake {
-    fn type_info() -> <MySql as sqlx::TypeInfo>::TypeInfo {
+    fn type_info() -> <MySql as Database>::TypeInfo {
         <u64 as Type<MySql>>::type_info()
     }
 }
 
 impl Type<Postgres> for Snowflake {
-    fn type_info() -> <Postgres as sqlx::TypeInfo>::TypeInfo {
+    fn type_info() -> <Postgres as Database>::TypeInfo {
         <i64 as Type<Postgres>>::type_info()
     }
 }
 
 impl Type<Sqlite> for Snowflake {
-    fn type_info() -> <Sqlite as sqlx::TypeInfo>::TypeInfo {
+    fn type_info() -> <Sqlite as Database>::TypeInfo {
         <i64 as Type<Sqlite>>::type_info()
     }
 }
